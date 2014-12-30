100c0059d7dd6b2923d7f98e564bc82c900ed0e3
kanehira <kanehira@mi.t.u-tokyo.ac.jp>
2014-12-30 12:45:24 +0900

    add constructer

--- clustering/k_means.js
+++ clustering/k_means.js
@@ -1,6 +1,16 @@
-(function(){
+(function() {
+    if (typeof AgentSmithML !== 'undefined' && typeof AgentSmithML.Kmeans !== 'undefined') {
+        return;
+    }
+    AgentSmithML = {};
+    AgentSmithML.Kmeans = function(n_clusters) {
+	if(n_clusters === undefined) n_clusters = 8;
+	this.n_clusters = n_clusters;
+    };
+    //var $M = AgentSmith.Kmeans;    
     var $M = AgentSmith.Matrix;
-    function k_means(n_cls){
+
+    AgentSmithML.Kmeans.prototype.fit = function(){
 	console.log("====== initialize ======");
 	var samples = $M.fromArray([
 	    [1, 1, 3],
@@ -18,22 +28,24 @@
 	var n_samples = samples.rows;
 	var n_features = samples.cols;
 	console.log("n_samples : " + n_samples + ", n_features : " + n_features);
-	var init_mats = init_clusters(n_cls, samples);
+	var init_mats = init_clusters(this.n_clusters, samples);
 	this.mean_class = init_mats[0];
 	this.assigned_class = init_mats[1];
 	this.datanum_hist = init_mats[2];
 
-	console.log("====== start calculating k-means ======")
+	console.log("====== start calculating k-means ======");
+	var sample_index_list = _.shuffle(_.range(n_samples));
 	for(var i=0; i<n_samples; i++){
+	    var sample_ind = sample_index_list[i];
 	    console.log("sample (%d / %d)", i, n_samples-1);
-	    if(this.assigned_class.data[i] != -1){
+	    if(this.assigned_class.data[sample_ind] != -1){
 		console.log("###skip calculation###");
 		continue;
 	    }
-	    sample = get_row(samples, i);
+	    sample = get_row(samples, sample_ind);
 	    var euc_dist = euclidian_distance(this.mean_class, sample);
 	    var cls_ind = $M.argminEachCol(euc_dist).data[0];
-	    this.assigned_class.data[i] = cls_ind;
+	    this.assigned_class.data[sample_ind] = cls_ind;
 	    var new_cls_mean = calc_new_mean(n_features, this.mean_class, this.datanum_hist, sample, cls_ind);
 	    set_row(this.mean_class, new_cls_mean, cls_ind);
 	    this.datanum_hist.data[cls_ind] += 1;
@@ -56,24 +68,23 @@
 	return new_cls_mean
     }
 
-    function init_clusters(n_cls, samples){
+    function init_clusters(n_clusters, samples){
 	console.log("initializing cluster ...")
 	var n_samples = samples.rows;
 	var n_features = samples.cols;
 	var assigned_class = new $M(n_samples, 1);
-	var mean_class = new $M(n_cls, n_features);
-	var datanum_hist = new $M(n_cls, 1);
+	var mean_class = new $M(n_clusters, n_features);
+	var datanum_hist = new $M(n_clusters, 1);
 	assigned_class.zeros(-1);
 	mean_class.zeros();
 	datanum_hist.zeros();
-
-//	var init_sample_ind = _.sample(_.range(n_samples), n_cls);
+//	var init_sample_ind = _.sample(_.range(n_samples), n_clusters);
 	var init_sample_ind = [0 , 8];
-	for(var c=0; c<n_cls; c++){ 
+	for(var c=0; c<n_clusters; c++){ 
 	    var index = init_sample_ind[c];
 	    assigned_class.data[index] = c;
 	    datanum_hist[c] += 1;
-	    set_row(mean_class, get_row(samples, index), c)
+	    set_row(mean_class, get_row(samples, index), c);
 	}
 	return [mean_class, assigned_class, datanum_hist]
     }
@@ -82,7 +93,7 @@
     function get_row(samples, i){
 	cols = samples.cols;
 	newArr = new $M(1, cols);
-	newArr.zeros()
+	newArr.zeros();
 	for(var j=0; j<cols; j++){
 	    newArr.data[j] = samples.data[i*cols + j];
 	}
@@ -99,11 +110,19 @@
 
     function euclidian_distance(mat1, mat2){
 	//console.log("EUC" + mat1 + '\n' + mat2 + '\n' + $M.sub(mat1, mat2));
-	var submat = $M.sub(mat1, mat2)
-	var dist = $M.sumEachRow($M.mulEach(submat, submat))
+	var submat = $M.sub(mat1, mat2);
+	var dist = $M.sumEachRow($M.mulEach(submat, submat));
 	return dist
-	
     }
 
-    k_means(2);
-})()
+    var kmeans = new AgentSmithML.Kmeans(2);
+    kmeans.fit();
+
+})();
+
+var nodejs = (typeof window === 'undefined');
+if (nodejs) {
+    module.exports = AgentSmith;
+}
+
+
