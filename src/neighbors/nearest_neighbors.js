619cb0bf57752873523815a42bfa9897d170749b
Yuichiro TCY <t.yic.yt@gmail.com>
2015-01-11 17:02:03 +0900

    Add tests

---
+++ src/neighbors/nearest_neighbors.js
@@ -0,0 +1,48 @@
+var nodejs = (typeof window === 'undefined');
+
+if (nodejs) {
+    var AgentSmithML = require('../agent_smith_ml');
+    require('./neighbors.js');
+}
+
+var $M = AgentSmith.Matrix;
+
+AgentSmithML.Neighbors.NearestNeighbors = function(n_neighbors, radius, algorithm, leaf_size) {
+    this.options = {};
+    this.options.n_neighbors = typeof n_neighbors === 'undefined' ? 5      : n_neighbors;
+    this.options.radius      = typeof radius      === 'undefined' ? 1.0    : radius;
+    this.options.algorithm   = typeof algorithm   === 'undefined' ? 'auto' : algorithm;
+    this.options.leaf_size   = typeof leaf_size   === 'undefined' ? 30     : leaf_size;
+}
+
+AgentSmithML.Neighbors.NearestNeighbors.prototype.fit = function(X, y) {
+    if (typeof X === 'undefined') throw new Error('X must be set');
+    if (!(X instanceof $M)) throw new Error('X must be an instance of AgentSmith.Matrix');
+    this.X = X;
+    this.y = typeof y === 'undefined' ? null : y;
+
+    // TODO
+    if (this._fit_method === 'auto') {
+        this._fit_method = 'brute';
+    }
+
+    return this.options;
+}
+
+AgentSmithML.Neighbors.NearestNeighbors.prototype.kneighbors = function(X) {
+    if (!(X instanceof $M)) throw new Error('X must be an instance of AgentSmith.Matrix');
+
+    if (this._fit_method === 'brute') {
+        
+
+    } else {
+        throw new Error('Invalid algorithm specified');
+    }
+
+    return $M.fromArray([[2],[0]]);
+}
+
+AgentSmithML.Neighbors.NearestNeighbors.prototype.radius_neighbors = function(X) {
+    if (!(X instanceof $M)) throw new Error('X must be an instance of AgentSmith.Matrix');
+    return $M.fromArray([[2]]);
+}
---
+++ tests/neighbors/nearest_neighbors_test.js
@@ -0,0 +1,73 @@
+var nodejs = (typeof window === 'undefined');
+if (nodejs) {
+    var TestMain = require('./main');
+    var AgentSmith = require('../agent_smith/src/agent_smith');
+    require('../agent_smith/src/agent_smith_cl');
+    require('../src/neighbors/neighbors.js');
+    require('../src/neighbors/nearest_neighbors.js');
+}
+
+var samples = $M.fromArray([[0, 0, 1.3], [1, 0, 0], [0, 0, 1]]);
+
+TestMain.Tester.addTest('NearestNeighborTest', [
+    {
+        name : 'Fit',
+        test : function() {
+            var neigh = new AgentSmithML.Neighbors.NearestNeighbors(2, 0.4);
+            var res = neigh.fit(samples);
+
+            if ( (typeof res.algorithm !== 'undefined' && res.algorithm === 'auto')
+                && (typeof res.leaf_size !== 'undefined' && res.leaf_size == 30)
+                && (typeof res.n_neighbors !== 'undefined' && res.n_neighbors == 2)
+                && (typeof res.radius !== 'undefined' && res.radius == 0.4)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        name: 'kneighbors',
+        test: function() {
+            var neigh = new AgentSmithML.Neighbors.NearestNeighbors(2, 0.4);
+            neigh.fit(samples);
+
+            var res = neigh.kneighbors($M.fromArray([0, 0, 1.3]), 2, {'return_distance': false});
+            return $M.fromArray([[2],[0]]).equals(res)
+        }
+    },
+    {
+        name: 'radius_neighbors',
+        test: function() {
+            var neigh = new AgentSmithML.Neighbors.NearestNeighbors(2, 0.4);
+            neigh.fit(samples);
+
+            var res = neigh.radius_neighbors($M.fromArray([0, 0, 1.3], 0.4, {'return_distance': false}));
+            return $M.fromArray([[2]]).equals(res);
+        }
+    },
+    {
+        name: 'Functions_throw_exception_if_invalid_instance_given',
+        test: function() {
+            var neigh = new AgentSmithML.Neighbors.NearestNeighbors(2, 0.4);
+            try {
+                neigh.fit($M.toArray(samples));
+                return false;
+            } catch(e) {}
+
+            neigh.fit(samples);
+
+            try {
+                neigh.kneighbors([0, 0, 1.3], 2, {'return_distance': false});
+                return false;
+            } catch(e) {}
+
+            try {
+                neigh.radius_neighbors([0, 0, 1.3], 0.4, {'return_distance': false});
+                return false;
+            } catch(e) {}
+
+            return true;
+        }
+    }
+]);
