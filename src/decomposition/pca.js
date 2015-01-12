e69fc71fcd65d95b58f506a1212ded17dc7d21e1
kanehira <kanehira@mi.t.u-tokyo.ac.jp>
2015-01-12 14:17:07 +0900

    change test

--- src/decomposition/pca.js
+++ src/decomposition/pca.js
@@ -9,12 +9,9 @@ if (nodejs) {
 var $M = AgentSmith.Matrix;
 
 AgentSmithML.Decomposition.PCA = function(n_components, copy, whiten) {
-    if(n_components === undefined) n_components = 10;
-    if(copy === undefined) copy = false;
-    if(whiten === undefined) whiten = false;
-    this.n_components = n_components;
-    this.copy = copy;
-    this.whiten = whiten;
+    this.n_components = typeof n_components === "undefined" ? 1.0 : n_components;
+    this.copy = typeof copy === "undefined" ? false : copy
+    this.whiten = typeof whiten === "undefined" ? false : whiten
     };
 
 AgentSmithML.Decomposition.PCA.prototype.fit = function(X, y){
@@ -31,8 +28,9 @@ AgentSmithML.Decomposition.PCA.prototype.fit = function(X, y){
     var explained_variance_ = $M.mulEach(S, S).times( 1.0 / n_samples );
     var explained_variance_ratio_ = (explained_variance_ / $M.sum(explained_variance_));
 
-    if(this.whiten){
-	""" TODO"""
+    if(this.whiten == true){
+	console.log("AA")
+	//""" TODO"""
         //components_ = V / (S[:, np.newaxis] / sqrt(n_samples))
     }
     else{
@@ -41,21 +39,22 @@ AgentSmithML.Decomposition.PCA.prototype.fit = function(X, y){
 
     
     var n_components = this.n_components;
-    if( isNan(n_components) ){
+    if( isNaN(n_components) ){
 	n_components = n_features;
     }
     else if( n_components == "mle" ){
 	if( n_samples < n_features ){
 	    throw new Error("n_components='mle' is only supported if n_samples >= n_features");
 	}
+	throw new Error("'mle' is not supported")
         n_components = _infer_dimension_(explained_variance_, n_samples, n_features);
     }
     else if(!(0 <= n_components <= n_features)){
         throw new ValueError("n_components=" + n_components + "invalid for n_features=" + n_features);
     }
 
-    if(0 < n_components < 1.0){
-	var ratio_cumsom = 0;
+    if(0 < n_components <= 1.0){
+	var ratio_cumsum = 0;
 	var components_ratio = n_components;
 	n_components = 0;
 	for(var i=0; i<explained_variance_ratio_.length; i++){
@@ -68,15 +67,16 @@ AgentSmithML.Decomposition.PCA.prototype.fit = function(X, y){
     }
 
     if(n_components < n_features){
-	""" TODO """
+	//""" TODO """
         //this.noise_variance_ = explained_variance_[n_components:].mean();
     }
     else{
 	this.noise_variance_ = 0.;
     }
     
-    """ TODO """
+    //""" TODO """
     this.n_samples_ = n_samples
+    this.components_ = components_
     //this.components_ = components_[:n_components]
     //this.explained_variance_ = explained_variance_[:n_components]
     //explained_variance_ratio_ = explained_variance_ratio_[:n_components]
@@ -85,7 +85,18 @@ AgentSmithML.Decomposition.PCA.prototype.fit = function(X, y){
 
 }
 
+AgentSmithML.Decomposition.PCA.prototype.transform = function(X){
+    if(typeof this.mean_ === "undefined"){
+        X = $M.sub(X,this.mean_);
+    }
+
+    X = $M.mul(X, this.components_.t())
+    return X
+}
+
+
 function _infer_dimension_(explained_variance_, n_samples, n_features){
     throw new Error("not implemented");
 }
+
 
