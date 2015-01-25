bc83f2e77fb5feb9035489de61069776e5d45aeb
Yuichiro TCY <t.yic.yt@gmail.com>
2015-01-25 23:50:06 +0900

    Add arguments input system to demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,15 +1,29 @@
 var AgentSmithDemo = {};
 
-(function($M){
+(function($M, $T, $){
 	AgentSmithDemo = {
 		demos: {},
-		initializer: function(){},
+		class_input_group: "input-group",
+		class_arg_textarea: "arg-input",
 		
-		addDemo: function(title, description, demo) {
+		setDrawArea: function($draw_area) {
+			this.$draw_area = $draw_area;
+		},
+		
+		addDemo: function(title, description, demo, demo_) {
+			var demo_func, args;
+			if (typeof demo_ === 'function') {
+				demo_func = demo_;
+				args = demo;
+			} else {
+				demo_func = demo;
+				args = {};
+			}
 			this.demos[title] = {
 				title: title,
 				description: description,
-				demo: demo
+				demo: demo_func,
+				args: args
 			}
 		},
 		
@@ -21,20 +35,80 @@ var AgentSmithDemo = {};
 			return this.demos[name].description;
 		},
 		
-		setInitializer: function(initFunc) {
-			this.initializer = initFunc;
+		runDemo: function(name) {
+			if (!this.$draw_area) {
+				console.log('Draw area not set');
+				return;
+			}
+			
+			console.log('Run: '+name);
+			
+			// Create plotter object
+			this.$draw_area.empty();
+			var plt = new $T("#" + this.$draw_area.attr("id"));
+			
+			// Set default arguments
+			var args_settings = this.demos[name].args;
+			var args = {};
+			for (var arg_name in args_settings) {
+				args[arg_name] = 'default' in args_settings[arg_name] ? args_settings[arg_name].default : null;
+			}
+			
+			// Run demo
+			this.demos[name].demo(plt, args);
 		},
 		
-		runDemo: function(name) {
+		runDemoByInput: function($input, name) {
+			if (!this.$draw_area) {
+				console.log('Draw area not set');
+				return;
+			}
+			
 			console.log('Run: '+name);
-			this.initializer();
-			this.demos[name].demo();
+			
+			// Create plotter object
+			this.$draw_area.empty();
+			var plt = new $T("#" + this.$draw_area.attr("id"));
+			
+			var args = AgentSmithDemo.getArgsFromInput($input, name);
+			this.demos[name].demo(plt, args);
+		},
+		
+		getArgsFromInput: function($input, name) {
+			var args = {};
+			if (name in this.demos) {
+				for (var arg_name in this.demos[name].args) {
+					args[arg_name] = $M.fromArray(JSON.parse($input.find("#arg-" + arg_name).val()));
+				}
+			}
+			return args;
 		},
 
 		getCode: function(name) {
 			return this.demos[name].demo.toString();
+		},
+		
+		setArgsInput: function($input, name) {
+			$input.empty();
+			if (name in this.demos) {
+				for (var arg_name in this.demos[name].args) {
+					var arg = this.demos[name].args[arg_name].default;
+					var $arg_group = $("<div>").addClass(this.class_input_group);
+					$arg_group
+						.append(
+							$("<p>").text(arg_name)
+						)
+						.append(
+							$("<textarea>").addClass(this.class_arg_textarea).attr("id", "arg-"+arg_name).val(JSON.stringify($M.toArray(arg)))
+						)
+					;
+					$input.append(
+						$arg_group
+					);
+				}
+			}
 		}
 	}
 	
-})(AgentSmith.Matrix);
+})(AgentSmith.Matrix, Trinity, jQuery);
 	
--- demo/js/demos/sgd_regressor.js
+++ demo/js/demos/sgd_regressor.js
@@ -1,83 +1,97 @@
-AgentSmithDemo.addDemo('SGDRegressor', 'A kind of linear classifier', function(){
-	var $M = AgentSmith.Matrix;
-	var $T = Trinity;
-	var $S = Neo.Utils.Statistics;
+(function($M){
+	AgentSmithDemo.addDemo('SGDRegressor',
+	'A kind of linear classifier',
+	{
+		X: {
+			shape: [2, 'n_data'],
+			default: $M.fromArray([
+					[ 1.4949318 ,  3.85848832],
+					[ 1.42613574,  0.0456529 ],
+					[ 1.1641107 ,  3.79132988],
+					[ 1.54632313,  4.212973  ],
+					[ 2.09680487,  3.7174206 ],
+					[ 1.24258802,  4.50399192],
+					[ 0.91433877,  4.55014643],
+					[ 2.14823598,  1.12456117],
+					[ 3.4171203 ,  0.02504426],
+					[-0.55552381,  4.69595848],
+					[ 2.08272263,  0.78535335],
+					[ 1.52259607, -0.29081422],
+					[ 2.97493505,  1.77927892],
+					[ 1.06269622,  5.17635143],
+					[ 1.82287143,  0.71628201],
+					[ 2.79344193,  1.61909157],
+					[ 1.84652023,  0.99147304],
+					[ 1.03150541,  2.0681289 ],
+					[ 1.87271752,  4.18069237],
+					[ 1.43289271,  4.37679234]
+				])
+		},
+		labels: {
+			shape: [1, 'n_data'],
+			default: $M.fromArray([
+					[1, 0],
+					[0, 1],
+					[1, 0],
+					[1, 0],
+					[1, 0],
+					[1, 0],
+					[1, 0],
+					[0, 1],
+					[0, 1],
+					[1, 0],
+					[0, 1],
+					[0, 1],
+					[0, 1],
+					[1, 0],
+					[0, 1],
+					[0, 1],
+					[0, 1],
+					[0, 1],
+					[1, 0],
+					[1, 0]
+				])
+		}
+	},
+	function(plt, args){
+		var $S = Neo.Utils.Statistics;
 
-	var samples = $M.fromArray([
-	    [ 1.4949318 ,  3.85848832],
-	    [ 1.42613574,  0.0456529 ],
-	    [ 1.1641107 ,  3.79132988],
-	    [ 1.54632313,  4.212973  ],
-	    [ 2.09680487,  3.7174206 ],
-	    [ 1.24258802,  4.50399192],
-	    [ 0.91433877,  4.55014643],
-	    [ 2.14823598,  1.12456117],
-	    [ 3.4171203 ,  0.02504426],
-	    [-0.55552381,  4.69595848],
-	    [ 2.08272263,  0.78535335],
-	    [ 1.52259607, -0.29081422],
-	    [ 2.97493505,  1.77927892],
-	    [ 1.06269622,  5.17635143],
-	    [ 1.82287143,  0.71628201],
-	    [ 2.79344193,  1.61909157],
-	    [ 1.84652023,  0.99147304],
-	    [ 1.03150541,  2.0681289 ],
-	    [ 1.87271752,  4.18069237],
-	    [ 1.43289271,  4.37679234]
-	]);
-	var labels = $M.fromArray([[1, 0],
-							   [0, 1],
-							   [1, 0],
-							   [1, 0],
-							   [1, 0],
-							   [1, 0],
-							   [1, 0],
-							   [0, 1],
-							   [0, 1],
-							   [1, 0],
-							   [0, 1],
-							   [0, 1],
-							   [0, 1],
-							   [1, 0],
-							   [0, 1],
-							   [0, 1],
-							   [0, 1],
-							   [0, 1],
-							   [1, 0],
-							   [1, 0]]);
+		var samples = args.X;
+		var labels = args.labels;
 
-	// fit neo
-	var per = new Neo.LinearModel.SGDRegressor({algorithm:'perceptron',aver:false,lambda:0.0});
-	var svm = new Neo.LinearModel.SGDRegressor({algorithm:'sgdsvm'});
-	per.fit(samples,labels);
-	svm.fit(samples,labels);
-	// weight output
-	var w = svm.weight;
-	w.print();
-	console.log( 'gradient' ); var grad = - w.get(0,0) / w.get(1,0); console.log( grad );
-	console.log( 'intercept' ); var inter = - w.get(2,0) / w.get(1,0); console.log( inter );
-	// init trinity
-	var plt = new $T("#content");
-	// sample
-	var meanStd = $S.meanStd( true, true, samples, false, 1);
-	var x = $M.extract(meanStd.X, 0, 0, samples.rows, 1);
-	var y = $M.extract(meanStd.X, 0, 1, samples.rows, 1);
-	var color = $M.fromArray([[0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0]]).t();
-	// line 1
-	var w1=per.weight.get(0,0), w2=per.weight.get(1,0), b=per.weight.get(2,0);
-	var p_line_x = new $M(60, 1);
-	p_line_x.setEach(function(i){ return -2 + i*0.1; });
-	var p_line_y = p_line_x.clone();
-	p_line_y.map(function(x) { return -w1/w2*x-b/w2;});
-	// line 2
-	var w1=svm.weight.get(0,0), w2=svm.weight.get(1,0), b=svm.weight.get(2,0);
-	var s_line_x = new $M(60, 1);
-	s_line_x.setEach(function(i){ return -2 + i*0.1; });
-	var s_line_y = s_line_x.clone();
-	s_line_y.map(function(x) { return -w1/w2*x-b/w2;});
-	// draw
-	plt.scatter(x,y,color);
-	plt.plot(s_line_x, s_line_y, 'r-');
-	plt.plot(p_line_x, p_line_y, 'b-');
-	plt.show();
-});
+		// fit neo
+		var per = new Neo.LinearModel.SGDRegressor({algorithm:'perceptron',aver:false,lambda:0.0});
+		var svm = new Neo.LinearModel.SGDRegressor({algorithm:'sgdsvm'});
+		per.fit(samples,labels);
+		svm.fit(samples,labels);
+		// weight output
+		var w = svm.weight;
+		w.print();
+		console.log( 'gradient' ); var grad = - w.get(0,0) / w.get(1,0); console.log( grad );
+		console.log( 'intercept' ); var inter = - w.get(2,0) / w.get(1,0); console.log( inter );
+		
+		// sample
+		var meanStd = $S.meanStd( true, true, samples, false, 1);
+		var x = $M.extract(meanStd.X, 0, 0, samples.rows, 1);
+		var y = $M.extract(meanStd.X, 0, 1, samples.rows, 1);
+		var color = $M.fromArray([[0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0]]).t();
+		// line 1
+		var w1=per.weight.get(0,0), w2=per.weight.get(1,0), b=per.weight.get(2,0);
+		var p_line_x = new $M(60, 1);
+		p_line_x.setEach(function(i){ return -2 + i*0.1; });
+		var p_line_y = p_line_x.clone();
+		p_line_y.map(function(x) { return -w1/w2*x-b/w2;});
+		// line 2
+		var w1=svm.weight.get(0,0), w2=svm.weight.get(1,0), b=svm.weight.get(2,0);
+		var s_line_x = new $M(60, 1);
+		s_line_x.setEach(function(i){ return -2 + i*0.1; });
+		var s_line_y = s_line_x.clone();
+		s_line_y.map(function(x) { return -w1/w2*x-b/w2;});
+		// draw
+		plt.scatter(x,y,color);
+		plt.plot(s_line_x, s_line_y, 'r-');
+		plt.plot(p_line_x, p_line_y, 'b-');
+		plt.show();
+	});
+})(AgentSmith.Matrix);
+
