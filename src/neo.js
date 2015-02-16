d96a5692bb4a2594b09f881d1862a6a10ca7cb76
miura <miura@mi.t.u-tokyo.ac.jp>
2015-02-16 16:42:04 +0900

    remove neo

--- src/neo.js
+++
@@ -1,7 +0,0 @@
-var Tempura = {};
-
-if (typeof window === 'undefined') {
-	(('global', eval)('this')).Tempura = Tempura;
-	require('../agent_smith/src/agent_smith_cl')
-	module.exports = Tempura;
-}
