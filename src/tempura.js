77d456c1817c53f559254730f21c2ae3c9cbe497
miura <miura@mi.t.u-tokyo.ac.jp>
2015-02-16 13:35:46 +0900

    agent_smith -> sushi

--- src/tempura.js
+++ src/tempura.js
@@ -2,6 +2,6 @@ var Tempura = {};
 
 if (typeof window === 'undefined') {
 	(('global', eval)('this')).Tempura = Tempura;
-	require('../agent_smith/src/agent_smith_cl')
+	require('../sushi/src/sushi_cl')
 	module.exports = Tempura;
 }
